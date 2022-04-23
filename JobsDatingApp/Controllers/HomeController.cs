f102da91705a608a2cb0cb32b313841437f65899
Винтер Яша <vinter-jasha@rambler.ru>
2022-04-23 19:01:59 +0700

    add Persistence

--- JobsDatingApp/Controllers/HomeController.cs
+++ JobsDatingApp/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace JobsDatingApp.Controllers
             _logger = logger;
             this.testViewModel = testViewModel;
         }
-
         public IActionResult Index()
         {
             return View();
---
+++ Persistence/Class1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Persistence
+{
+    public class ApplicationContext : DbContext
+    {
+        //public DbSet<User> Users => Set<User>();
+        //public DbSet<Person> Persons { get; set; } = null!;
+        //public DbSet<Address> Addresses { get; set; } = null!;
+        public string dbPath = @"C:\Users\User\source\repos\EntityFrameworkApp\EntityFrameworkApp\TelegramBotDB.db";
+        public ApplicationContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");// TelegramBotDB / TelegramBotDB
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Person>().HasIndex(a => a.name).IsUnique();
+        }
+    }
+    public class Test 
+    {
+        public void Test1() 
+        {
+            //using (ApplicationContext db = new ApplicationContext())
+            //{
+            //    //recreated Database
+            //    db.Database.EnsureDeleted();
+            //    db.Database.EnsureCreated();
+            //    // adding all persons in DbData
+            //    var persons = new List<Person>
+            //    {
+            //        DbData.Yan, DbData.Polina, DbData.Artem
+            //    };
+            //    db.Persons.AddRange(persons);
+            //    db.SaveChanges();
+            //}
+        }
+    }
+}
